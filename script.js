75a793ddac8359f18efc092900727c4acffb2f20
Gautam kumar Singh <146578992+Gautamkumarsinghh@users.noreply.github.com>
2026-02-16 12:55:47 +0530

    learn a inc or dec

---
+++ script.js
@@ -0,0 +1,23 @@
+// // // let c=4;
+// // // b=6;
+// // // console.log("a + b =", c + b);
+
+// // let a = 2;
+// // let b = "9";
+// // console.log(a=b)
+// // length
+
+// // let text = "hello";
+// // console.log(text.toUpperCase());  // HELLO
+
+// // let text = "Hello";
+// // console.log(text.length);   // 5
+
+// const a ="morning in good happy endingh";
+// console.log(a.length)
+
+let i=1;
+for(i=1;i<=5; i++){
+    console.log(i);
+}
+
