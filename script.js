00ed0257d6cd537f11d6ecbe3e935977d52801fb
Gautam kumar Singh <146578992+Gautamkumarsinghh@users.noreply.github.com>
2026-02-17 12:47:06 +0530

    is else statement learn of js

--- script.js
+++ script.js
@@ -29,4 +29,57 @@
 //     console.log("you cannot vote");
 //  }
 
+//conditional statement 
 
+// let mode = "light";
+// let colour ;
+
+//  if ( mode === "dark"){
+//     colour ="black";
+//  }
+//   if ( mode === "light"){
+//     colour ="pink";
+//  }
+// console.log(colour);
+
+//else-if statment
+// let age = "18";
+// if(age <18){
+//     console.log("junior");
+    
+// } else if(age>60){
+//     console.log("senior");
+// } else {
+//     console.log("middle")
+// }
+//ternary operators
+// let age= 16;
+// let a = age >=18?"adult" : "not adult";
+// console.log(a)
+
+//partice Q1
+
+// let a = prompt("enter number");
+// if (a % 5 === 0){
+//     console.log(a,"is multiple of 5");
+// }
+// else{
+//     console.log(a,"is not multiple of 5");
+// }
+
+//partice Q2
+let score =prompt("enter your score(0-100");
+let grade;
+
+if(score >=90 && score <= 100){
+    grade = "A";
+} else if(score >= 70 && score <=89){
+    grade ="B";
+}else if(score >= 60 && score <=69){
+    grade ="C";
+}else if(score >= 50 && score <=59){
+    grade ="D";
+}else if(score >= 0 && score <=49){
+    grade ="F";
+}
+console.log("according to your scores,Your grade was:",grade)
