438344820e95b8e697c91da84d702b9c29685a56
Gautam kumar Singh <146578992+Gautamkumarsinghh@users.noreply.github.com>
2026-04-08 12:57:40 +0530

    next

--- function.js
+++ function.js
@@ -11,9 +11,50 @@
 
                                 //Arrow function
              
- const a=5;                              
- const sum = (a,b) =>{
-    console.log(a+b) ;
+//  const a=5;                              
+//  const sum = (a,b) =>{
+//     console.log(a+b) ;
 
-};
-kk
+// };
+
+// function countvowel(str){
+//     let count = 0;
+//     for(const char of str){
+//         if(
+//             char === "a" || char === "e" || char ==="i" || char === "o" || char === "u"
+//         )
+//         {
+//             count++;
+//          }
+//     }
+//     console.log(count);
+
+// }
+ 
+// find a vowel count in string
+// arrow function 
+// const count = (str) => {
+//     let count = 0;
+//     for(const char of str){
+//         if(
+//             char === "a" || char === "e" || char ==="i" || char === "o" || char === "u"
+//         )
+//         {
+//             count++;
+//          }
+//     }
+//     return count;
+// }
+
+// //forEach loop kay hai 
+// let fruits = ["apple", "banana", "guava"];
+
+// fruits.forEach(function(fruit, index, val){
+//     console.log(index + " : " + fruit,val);
+// });
+
+//find a square in number
+let num = prompt("enter a number : ");
+num.forEach((num) => {
+    console.log(num * num);
+})
